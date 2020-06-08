183b3e681ff3077b864351ad7a24127027c7df81
Paweł Biernat <pawel.biernat@ardigen.com>
2020-06-08 15:06:06 +0200

    Updated the config after upgrading to ubuntu 20.04

--- personal/python.el
+++ personal/python.el
@@ -1,20 +1,22 @@
+(elpy-enable)
+
 (require 'flycheck-mypy)
 
 (custom-set-variables
  '(flycheck-python-flake8-executable "python3")
  '(flycheck-python-pycompile-executable "python3")
  '(flycheck-python-pylint-executable "python3")
- '(flycheck-python-mypy-executable "mypy")
- '(importmagic-python-interpreter "python3")
+ '(flycheck-python-mypy-executable "python3")
  '(flycheck-python-mypy-args '("--ignore-missing-imports"))
+ '(importmagic-python-interpreter "python3")
  )
 
 
 
 (add-hook 'python-mode-hook (lambda ()
-                              (importmagic-mode)
-                              (flycheck-select-checker 'python-mypy)
-                              ;; (add-hook 'before-save-hook 'pyimport-remove-unused)
+                              ;; (importmagic-mode)
+                              ;; (flycheck-select-checker 'python-mypy)
+                              ;; (add-hook 'before-save-hook 'pyimpsort-buffer)
                               ;; (add-hook 'before-save-hook 'importmagic-fix-imports)
-                              (add-hook 'before-save-hook 'blacken-buffer)
+                              (add-hook 'before-save-hook 'blacken-buffer nil 'make-it-local)
                               ))
