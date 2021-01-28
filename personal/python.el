da68fb5b725ba5e946835c1326e3fee6ce500022
Paweł Biernat <pawel.biernat@ardigen.com>
2021-01-28 09:10:15 +0100

    Update

--- personal/python.el
+++ personal/python.el
@@ -6,9 +6,13 @@
  '(flycheck-python-flake8-executable "python3")
  '(flycheck-python-pycompile-executable "python3")
  '(flycheck-python-pylint-executable "python3")
- '(flycheck-python-mypy-executable "python3")
+ '(flycheck-python-mypy-executable "mypy")
  '(flycheck-python-mypy-args '("--ignore-missing-imports"))
+ '(flycheck-disabled-checkers '(python-pylint))
  '(importmagic-python-interpreter "python3")
+ '(elpy-rpc-python-command "python3")
+ '(elpy-rpc-virtualenv-path 'system)
+ ;; '(blacken-line-length 88)
  )
 
 
